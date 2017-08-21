4f3c7b20a32178b182911ff75f7f94f14513bce0
Michael Centrelli <Acentrelli@gmail.com>
2017-08-21 17:03:59 -0600

    Hide API keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :github, "260423c5e6547f01bc8f", "901ac26dff6ad9e57e0fe946359980a62d2add1b"
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
 end
+
