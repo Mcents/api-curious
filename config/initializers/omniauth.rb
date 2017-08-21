5a34b54d31208007539da4312a50a6961dc8ecf3
Michael Centrelli <Acentrelli@gmail.com>
2017-08-21 17:06:21 -0600

    Fixed Merge Conflict In Initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-<<<<<<< HEAD
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-=======
-    provider :github, "", ""
->>>>>>> 19fe9d2b56b99a2b45b93e5158985080d6c58ea8
 end
 
