2b09fe39995966d6d06eb77379cb35204b5cc13f
Michael Centrelli <Acentrelli@gmail.com>
2017-08-24 13:27:54 -0600

    Push Star Repo Logic to Model

--- app/models/starred_repo.rb
+++ app/models/starred_repo.rb
@@ -1,10 +1,12 @@
  class StarredRepo
+   attr_reader :repo_name
    
   def initialize(star_rep={})
+    @repo_name = star_rep[:name]
   end
 
   def self.star_rep(user)
-    GithubService.new(user).starred_repo.each do |com|
+    GithubService.new(user).starred_repo.map do |com|
       StarredRepo.new(com)
     end
   end
