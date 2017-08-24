a6199de4a699aec35f665a62f1a946fb74ddfb6b
Michael Centrelli <Acentrelli@gmail.com>
2017-08-24 11:19:09 -0600

    Pushed Repo Logic Into Model

--- app/models/github_user_repo.rb
+++ app/models/github_user_repo.rb
@@ -1,12 +1,14 @@
 class GithubUserRepo
-  attr_reader :repo_name
+  attr_reader :repo_name,
+              :html_url
 
   def initialize(user_repo_atts={})
     @repo_name = user_repo_atts[:name]
+    @html_url = user_repo_atts[:html_url]
   end
 
   def self.user_repo(user)
-    GithubService.new(user).user_repo.each do |com|
+    GithubService.new(user).user_repo.map do |com|
       GithubUserRepo.new(com)
     end
   end
