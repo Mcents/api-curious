a1a5c8561405a6be0845d1b916ee72d6d51411a5
Michael Centrelli <Acentrelli@gmail.com>
2017-08-24 13:26:13 -0600

    Pushed Follower Logic to Model

--- app/models/follower_commits.rb
+++ app/models/follower_commits.rb
@@ -1,10 +1,12 @@
 class FollowerCommits
+  attr_reader :repo_name
 
   def initialize(user_followers={})
+    @repo_name = user_followers[:repo][:name]
   end
 
   def self.user_followers(user)
-    GithubService.new(user).follower_commits.each do |com|
+    GithubService.new(user).follower_commits.map do |com|
       FollowerCommits.new(com)
     end
   end
